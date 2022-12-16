c0a22aaa44e7ed1bfcd0772202d161da88c0ee0c
GH0ST <adamjamessherrill@gmail.com>
2022-12-15 18:13:23 -0600

    debugged database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,9 +18,9 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ value: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('data saved to the database');
+  console.log('data saved to the database', result.value);
 }
 
 
@@ -30,10 +30,10 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result.value);
-  return result;
+  return result?.value;
 }
 
 initdb();
